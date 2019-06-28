4a481e2a33277da08249cdd9a66c2bc1e8b55f43
Antony Jones <aj@desirableobjects.co.uk>
2019-06-28 21:22:22 +0100

    resolve stupid path issues

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ScrollableSection from './ScrollableSection.svelte'
+import ScrollSpy from './ScrollSpy.svelte'
+import SectionHeader from './SectionHeader.svelte'
+
+export {
+  ScrollSpy,
+  ScrollableSection,
+  SectionHeader
+}
